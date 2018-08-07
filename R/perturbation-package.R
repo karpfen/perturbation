b4e6578b15bb31e75cd150221b5fad8340e72bbb
Andreas Petutschnig <andreas@petutschnig.de>
2018-08-07 17:23:30 +0200

    add animation

--- R/perturbation-package.R
+++ R/perturbation-package.R
@@ -4,6 +4,8 @@
 #' @docType package
 #' @importFrom ggplot2 aes ggplot geom_boxplot geom_point scale_x_continuous scale_y_continuous
 #' @importFrom graphics boxplot
+#' @importFrom grDevices dev.off
+#' @importFrom magick image_animate image_graph image_write
 #' @importFrom stats rnorm runif
 #' @importFrom utils stack
 NULL
--- R/plot_animation.R
+++ R/plot_animation.R
@@ -3,10 +3,16 @@
 #' Generate an animated gif from a list of ggplot objects.
 #'
 #' @param plots \code{list} of ggplot objects.
-#' @param fps frames per second.
 #' @param file_out output file location.
+#' @param fps frames per second.
+#' @param res resolution in pixels.
 #'
 #' @export
-save_animated_plot <- function (plots, fps, file_out)
+save_animated_plot <- function (plots, file_out, fps = 10, res = 96)
 {
+  img <- magick::image_graph (res = res)
+  lapply (plots, print)
+  dev.off ()
+  animation <- magick::image_animate(img, fps = fps)
+  image_write(animation, file_out)
 }
